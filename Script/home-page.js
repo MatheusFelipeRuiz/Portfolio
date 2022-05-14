d5343526e20aa7c463958d11d48f166ff9c90f10
Matheus Felipe Ruiz Gonçalves <matheus57.ruiz100@gmail.com>
2022-05-13 21:14:11 -0300

    1.1

---
+++ Script/home-page.js
@@ -0,0 +1,42 @@
+
+
+
+{let linkedin = window.document.getElementById('linkedin')
+linkedin.addEventListener ('mouseenter', entrouLinkedin)
+linkedin.addEventListener ('mouseout', sairLinkedin)
+
+
+
+function entrouLinkedin(){let linkedin = window.document.getElementById('linkedin').src = "Src/imagens/linkedin interativo.png"}
+function sairLinkedin(){let linkedin = window.document.getElementById('linkedin').src = "Src/imagens/linkedin.png"}}
+
+{let instagram = window.document.getElementById('instagram')
+instagram.addEventListener ('mouseenter', entrouInstagram)
+instagram.addEventListener ('mouseout', sairInstagram)
+
+
+
+function entrouInstagram(){let Instagram = window.document.getElementById('instagram').src = "Src/imagens/instagram interativo.png"}
+function sairInstagram(){let Instagram = window.document.getElementById('instagram').src = "Src/imagens/instagram.png"}}
+
+{let github = window.document.getElementById('github')
+github.addEventListener ('mouseenter', entrouGithub)
+github.addEventListener ('mouseout', sairGithub)
+
+
+
+function entrouGithub(){let github = window.document.getElementById('github').src = "Src/imagens/github interativo.png"}
+function sairGithub(){let github = window.document.getElementById('github').src = "Src/imagens/github.png"}}
+
+{let pdf = window.document.getElementById('pdf')
+pdf.addEventListener ('mouseenter', entrouPdf)
+pdf.addEventListener ('mouseout', sairPdf)
+
+
+
+function entrouPdf(){let pdf = window.document.getElementById('pdf').src = "Src/imagens/pdf interativo.png"}
+function sairPdf(){let pdf = window.document.getElementById('pdf').src = "Src/imagens/pdf.png"}}
+
+
+
+
